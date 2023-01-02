f738567399351fe50b88d62f32dd8b7e2fe136ed
root <root@karafs-bug-report-panel.karafs>
2023-01-02 20:23:59 +0000

    Init

---
+++ config/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'mantisdb';
+$g_db_username            = 'amir';
+$g_db_password            = '1q2w3e4r';
+
+$g_default_timezone       = 'UTC';
+
+$g_crypto_master_salt     = 'iLKT7j9O9lOsSE+DrSECW+AY7fVGCsUlEABVEf8ENNU=';
